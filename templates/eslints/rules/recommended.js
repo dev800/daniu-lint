421cb2403948a31bcc9c7b5c91422b44d286d1eb
WangJinzhong <andywang7259@163.com>
2020-12-15 14:01:35 +0800

    eslint fix

--- templates/eslints/rules/recommended.js
+++ templates/eslints/rules/recommended.js
@@ -5,7 +5,14 @@ module.exports = {
   'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 1, maxEOF: 1 }],
   '@typescript-eslint/no-namespace': 'off',
   '@typescript-eslint/ban-types': 'off',
-  'space-before-function-paren': ['error', 'never'],
+  'space-before-function-paren': [
+    'error',
+    {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always',
+    },
+  ],
   '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   '@typescript-eslint/no-this-alias': [
     'error',
